95051a90d43c78d15227a7f16111e1436276dce6
Allen Chen <allen.chen@hiero7.com>
2022-12-30 15:09:18 +0800

    add template

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    args = {
+        'name': 'John',
+        'age': 21,
+        'vip': True
+    }
+    return render(request, './polls/index.html', args)
